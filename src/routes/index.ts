43c32e52ab1f308dae17c1a5668f307b2209cf23
Daniel Ribeiro <daniel.ribeiro.dev@outlook.com>
2021-08-14 11:25:03 -0300

    Criando addContact | Página

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { home, contact } from '../controllers/PageController';
+import { home, contact, addContact } from '../controllers/PageController';
 
 const router = Router();
 
 router.get('/', home); 
 router.get('/contact', contact);
+router.get('/add-contact', addContact);
 
 export default router;
